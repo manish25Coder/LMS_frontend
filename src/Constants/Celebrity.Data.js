d518c965421c0891bde2e0162b8203b0c39f8147
manish25Coder <manishsingh59766@gmail.com>
2023-11-29 18:37:34 +0530

    Cleaned up our about us page

---
+++ src/Constants/Celebrity.Data.js
@@ -0,0 +1,38 @@
+import kalam from "../assets/Images/kalam.jpeg"
+import Bill_Gates from "../assets/Images/Bill_Gates.jpeg"
+import einstein from "../assets/Images/einstein.jpg"
+import Nelson_mandela from "../assets/Images/Nelson_mandela.jpeg"
+import SteveJobs from "../assets/Images/SteveJobs.jpeg"
+
+export const celebrities = [
+    {
+         title :'Nelson Mandela',
+         description : "Education is most powerful tool you can use to change the world.",
+         image : Nelson_mandela,
+         slideNumber:1
+    },
+    {
+        title :' A.P.J Abdul Kalam',
+        description : "If you fail, never give up because FAIL means First Attempt In Learning.",
+        image : kalam,
+        slideNumber:2
+    },
+    {
+         title :'Einstein',
+         description : "Anyone who has never made a mistake has never tried anything new.",
+         image : einstein,
+         slideNumber:3
+    },
+    {
+        title :'Bill Gates',
+        description : "We all need people who will give us feedback. That’s how we improve.",
+        image : Bill_Gates,
+        slideNumber:4
+    },
+   {
+        title :'Steve Jobs',
+        description : "Design is not what it looks and feels like. Design is how it works.",
+        image : SteveJobs,
+        slideNumber:5
+    },
+]
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,12 +1,12 @@
 import HomeLayout from "../Layouts/HomeLayout"
 import About_image from '../assets/Images/About_image.png'
-import kalam from "../assets/Images/kalam.jpeg"
-import Bill_Gates from "../assets/Images/Bill_Gates.jpeg"
-import einstein from "../assets/Images/einstein.jpg"
-import Nelson_mandela from "../assets/Images/Nelson_mandela.jpeg"
-import SteveJobs from "../assets/Images/SteveJobs.jpeg"
+import { celebrities } from "../Constants/Celebrity.Data"
+import CarouselSlide from "../Components/CarouselSlide"
 
 function AboutUs() {
+
+   
+
     return(
         <HomeLayout>
             <div className="pl-20 pt-20 flex flex-col text-white">
@@ -27,7 +27,7 @@ function AboutUs() {
                         <img
                             id="test1"
                             style={{
-                                filter: "drop-shadow(0px 10px 10px rgb(0,0,0));"
+                                filter: "drop-shadow(0px 10px 10px rgb(0,0,0))"
                             }}
                             alt="About iamge"
                             className="drop-shadow-2xl"
@@ -38,73 +38,14 @@ function AboutUs() {
                 </div>
 
                 <div className="carousel w-1/2 my-16 m-auto">
-                    <div id="slide1" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={kalam} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"If you fail, never give up because FAIL means First Attempt In Learning."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">A.P.J Abdul Kalam</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide5" className="btn btn-circle">❮</a> 
-                                <a href="#slide2" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        
-                    </div> 
-                    <div id="slide2" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={Bill_Gates} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"We all need people who will give us feedback. That’s how we improve."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Bill Gates</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide1" className="btn btn-circle">❮</a> 
-                                <a href="#slide3" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div> 
-                    <div id="slide3" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={einstein} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"Anyone who has never made a mistake has never tried anything new."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Einstein</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide2" className="btn btn-circle">❮</a> 
-                                <a href="#slide4" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div> 
-                    <div id="slide4" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={Nelson_mandela} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"Education is most powerful tool you can use to change the world."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide3" className="btn btn-circle">❮</a> 
-                                <a href="#slide5" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div> 
-                    <div id="slide5" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={SteveJobs} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"Design is not what it looks and feels like. Design is how it works"}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide4" className="btn btn-circle">❮</a> 
-                                <a href="#slide1" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    
-                    </div>
+                    {celebrities && celebrities.map(celebrity => (<CarouselSlide 
+                                                                        {...celebrity} 
+                                                                        key={celebrity.slideNumber} 
+                                                                        totalSlides={celebrities.length}/>
+                                                                        )
+                                                    )}
+                    <CarouselSlide/>
+                  
                     </div>
 
             </div>
